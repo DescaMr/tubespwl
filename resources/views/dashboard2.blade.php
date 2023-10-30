64a5c5ad7fed696f9d172fa25c89e328de506885
DescaMr <drizal07@gmail.com>
2023-10-30 13:56:09 +0700

    mengupdate dashboard2 untuk footer dan menambahkan file footer.blade.php

--- resources/views/dashboard2.blade.php
+++ resources/views/dashboard2.blade.php
@@ -23,6 +23,10 @@
   <x-sidenav/>
   <div class="w-full flex flex-col h-screen overflow-y-hidden"> 
   <x-headernav/>
+  <div class="w-full overflow-x-hidden border-t flex flex-col">
+
+  <x-footer/>
+  </div>
   </div>
   <!-- AlpineJS -->
   <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
